5f273a8165386cdf96debb9e6c5a0febf79f9096
Paladox <thomasmulhall410@yahoo.com>
2016-03-30 13:49:25 +0100

    empty main php entry point
    Change-Id: Ife6966965e949439607b8fddd69d797fea75ba2b

--- BaseHooks.php
+++ BaseHooks.php
@@ -1,19 +1,14 @@
 <?php
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
+
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'BaseHooks' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['BaseHooks'] = __DIR__ . '/i18n';
+	/* wfWarn(
+		'Deprecated PHP entry point used for BaseHooks extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return;
+} else {
+	die( 'This version of the BaseHooks extension requires MediaWiki 1.25+' );
 }
-
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'BaseHooks',
-	'version' => '0.0.1',
-	'author' => array( '[https://www.mediawiki.org/wiki/User:VictorPorton Victor Porton]' ),
-	'descriptionmsg' => 'basehooks-desc',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:BaseHooks',
-);
-
-$wgMessagesDirs['BaseHooks'] = __DIR__ . '/i18n';
-
-$wgAutoloadClasses['BaseHooksHooks'] = __DIR__ . '/BaseHooks.hooks.php';
-$wgHooks['SkinAfterBottomScripts'][] = 'BaseHooksHooks::onSkinAfterBottomScripts';
-$wgHooks['UnitTestsList'][] = 'BaseHooksHooks::onUnitTestsList';
