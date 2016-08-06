3c024ea99842532a8efc043fc1643d5218adc178
Kunal Mehta <legoktm@member.fsf.org>
2016-08-06 01:24:42 -0700

    Move tests to tests/phpunit/ and remove hook
    The UnitTestsList hook is no longer needed as tests are now
    autodiscovered.
    
    Bug: T142120
    Change-Id: Ia3b5bc84586922da40ed6b1114dcb0d2734d3d6f

--- BaseHooks.hooks.php
+++ BaseHooks.hooks.php
@@ -38,23 +38,4 @@ class BaseHooksHooks {
 
 		return true;
 	}
-
-	public static function onUnitTestsList( array &$files ) {
-		// @codeCoverageIgnoreStart
-		$directoryIterator = new RecursiveDirectoryIterator( __DIR__ . '/tests/' );
-
-		/**
-		 * @var SplFileInfo $fileInfo
-		 */
-		$ourFiles = [];
-		foreach ( new RecursiveIteratorIterator( $directoryIterator ) as $fileInfo ) {
-			if ( substr( $fileInfo->getFilename(), -8 ) === 'Test.php' ) {
-				$ourFiles[] = $fileInfo->getPathname();
-			}
-		}
-
-		$files = array_merge( $files, $ourFiles );
-		return true;
-		// @codeCoverageIgnoreEnd
-	}
 }
